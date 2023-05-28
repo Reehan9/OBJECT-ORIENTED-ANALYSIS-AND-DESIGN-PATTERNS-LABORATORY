02df671f302e8cb2e8e0f922367889e42886fbb6
Reehan <92086803+Reehan9@users.noreply.github.com>
2023-05-28 18:32:14 +0530

    Add files via upload

---
+++ Lab/Fascade/Client.java
@@ -0,0 +1,20 @@
+package isl67_facade_ut;
+
+import java.util.*;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the bill amount : ");
+		double billAmount = sc.nextDouble();
+		System.out.print("\nDo you have a giftCard (true/false) : " );
+		boolean value = sc.nextBoolean();
+		Facade fc = new Facade(value, billAmount);
+		fc.calculateAmountToPay();
+		fc.displayItemsPurchased();
+		
+
+	}
+
+}
---
+++ Lab/Fascade/Facade.java
@@ -0,0 +1,26 @@
+package isl67_facade_ut;
+
+public class Facade {
+	
+	ItemPurchased I1;
+	ProcessSales P1;
+	
+	Facade(boolean giftCard, double billAmount){
+	I1 = new ItemPurchased();
+	I1.storePurchaseDetails();
+	int numItems = I1.getItemTypes();
+	System.out.println("Number of Items Purchased is " + numItems);
+	System.out.println("Gift Certificate exists " + giftCard);
+	P1 = new ProcessSales(giftCard, numItems, billAmount);
+	}
+	
+	void displayItemsPurchased() {
+	I1.getItemsPurchased();
+	}
+	
+	void calculateAmountToPay() {
+		P1.processSale();
+	}
+
+	
+}
