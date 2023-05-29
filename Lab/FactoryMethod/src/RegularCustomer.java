61992a486ca0dff077817d595a713c189ad66bae
Reehan <92086803+Reehan9@users.noreply.github.com>
2023-05-29 10:26:45 +0530

    Update RegularCustomer.java

--- Lab/FactoryMethod/src/RegularCustomer.java
+++ Lab/FactoryMethod/src/RegularCustomer.java
@@ -4,7 +4,7 @@ public class RegularCustomer implements Customer {
 	@Override
 	public void greet() {
 		// TODO Auto-generated method stub
-		System.out.println("Hello I am comming here for the first time");
+		System.out.println("Hello I am come here regularly");
 	}
 
 }
